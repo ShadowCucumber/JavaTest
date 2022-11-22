d0a6fc4e6c1c61965a0cb0685fc51f9e794c93d7
Mattis Henzel <111503864+ShadowCucumber@users.noreply.github.com>
2022-11-22 09:10:31 +0100

    Update Mensch.java

--- Zwischenstand/src/Mensch.java
+++ Zwischenstand/src/Mensch.java
@@ -1,10 +1,13 @@
 public class Mensch {
     String vorname;
     String nachname;
+    int alter;
 
-    public Mensch(String vorname, String nachname){
+    public Mensch(String vorname, String nachname, int alter){
         setNachname(nachname);
         setVorname(vorname);
+        setAlter(alter);
+
     }
 
 
@@ -20,5 +23,12 @@ public class Mensch {
     public String getVorname() {
         return vorname;
     }
+    public void setAlter(int alter) {
+        this.alter = alter;
+    }
+    public int getAlter() {
+        return alter;
+    }
+
     
 }
