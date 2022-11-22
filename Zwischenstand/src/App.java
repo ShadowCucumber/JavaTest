d0bba48a4c8d8ab27896d0486f54187ac8083846
FinnPogO <111505081+FinnPogO@users.noreply.github.com>
2022-11-22 09:24:00 +0100

    App

--- Zwischenstand/src/App.java
+++ Zwischenstand/src/App.java
@@ -1,7 +1,9 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+       
+        Mensch m1 = new Mensch("Carsten", "Müller", 38);
+        Mensch m2 = new Mensch("Scharlatanius", "Trump", 43);
+        Mensch m3 = new Mensch("Pfiffikuss", "Meier", 78);
 
-        System.out.println();
     }
 }
