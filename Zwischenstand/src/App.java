240bf78460200186b846d55348c851eb4049bd5d
Mattis Henzel <111503864+ShadowCucumber@users.noreply.github.com>
2022-12-20 09:08:32 +0100

    Update App.java
    Für Finn Abi (Für Fortnite)

--- Zwischenstand/src/App.java
+++ Zwischenstand/src/App.java
@@ -1,14 +1,20 @@
 import java.util.ArrayList;
 
+
+
 public class App {
     public static void main(String[] args) throws Exception {
        
         Mensch m1 = new Kunde (1,"Carsten", "Müller", 38,true);
         Mensch m2 = new Mitarbeiter(12,"Scharlatanius", "Trump", 43,false);
-       
+        Mensch m3 = new Kunde(72, "Daniel", "Trampe", 19, true);
+        Mensch m4 = new Einbrecher(23, "Holger", "Trampe", 98, false);
+        
         ArrayList<Mensch> menschen = new ArrayList<>();
         menschen.add(m1);
         menschen.add(m2);
+        menschen.add(m3);
+        menschen.add(m4);
 
         
         for (Mensch mensch : menschen) {
@@ -19,13 +25,21 @@ public class App {
             System.out.println("Der bre braucht Fahrstuhl");
         } else {
             System.out.println("Der bre kann treppen Laufen");
-            
+        
         }
         if (m2.getRollstuhl()) {
             System.out.println("Der bre braucht Fahrstuhl");
         } else {
             System.out.println("Der bre kann treppen Laufen");
         }
+
+        if (m4.getRollstuhl()) {
+            System.out.println("Der bre kann nicht mehr so gut einbrechen");
+        } else {
+            System.out.println("Der bre kann einbrechen");
+        }
+
+
 
         
     }
